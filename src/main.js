371afc65fa93e7af64186d93b963b1ea50a3e902
Mariia D <mariia.demidova0304@gmail.com>
2025-07-21 11:33:34 -0400

    Added event listener to form submission

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import { IpMatchingLocation } from "./models/Location.js";
 import { fetchUserIp, fetchLocationByIp } from "./services/apiService.js";
+const inputForm = document.querySelector(`.input-container`);
 const ipInputField = document.getElementById(`ip-input`);
 const locationDataBox = document.querySelector(`.location-data-container`);
 const mapBox = document.getElementById(`map`);
+
+
+inputForm.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    //mock if statement to check validity later
+    if (true) {
+        const ipToDisplay = ipInputField.value;
+        fetchLocationByIp(ipToDisplay)
+            .then((locationObject) => {
+                const locationObjectIp = locationObject.ip;
+                const locationDetails = locationObject.location;
+                const locationIsp = locationObject.isp;
+                return new IpMatchingLocation(locationObjectIp, locationDetails.country, locationDetails.region, locationDetails.city, locationDetails.lat, locationDetails.lng, locationDetails.postalCode, locationDetails.timezone, locationIsp);
+                //console.log(`Got this data from API`);
+                //console.log()
+            })
+            .then((myIpMatchingLocation) => console.log(myIpMatchingLocation))
+            .catch(error => { console.error(error); })
+            .finally(() => { console.log(`Location logged`); });
+    }
+})
 
 //we're going to be fetching user's ip on page load and creating a location object
 // maybe turn creating an object into a reusable function - inside location object as a method?
