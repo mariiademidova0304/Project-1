9f8c4a1d132b1a37e8a3c2bbf7833b7f872b1d0c
Mariia D <mariia.demidova0304@gmail.com>
2025-07-21 10:46:26 -0400

    added a mock Location object and an actual map

--- src/main.js
+++ src/main.js
@@ -1,19 +1,48 @@
 import { IpMatchingLocation } from "./models/Location.js";
 import { fetchUserIp, fetchLocationByIp } from "./services/apiService.js";
+const ipInputField = document.getElementById(`ip-input`);
+const locationDataBox = document.querySelector(`.location-data-container`);
+const mapBox = document.getElementById(`map`);
 
 //we're going to be fetching user's ip on page load and creating a location object
 // maybe turn creating an object into a reusable function - inside location object as a method?
-fetchUserIp().then((ipString) => {
-    return fetchLocationByIp(ipString);
-})
-    .then((locationObject) => {
-    const locationObjectIp = locationObject.ip;
-    const locationDetails = locationObject.location;
-    const locationIsp = locationObject.isp;
-    return new IpMatchingLocation(locationObjectIp, locationDetails.country, locationDetails.region, locationDetails.city, locationDetails.lat, locationDetails.lng, locationDetails.postalCode, locationDetails.timezone, locationIsp);
-    //console.log(`Got this data from API`);
-    //console.log()
-})
-    .then((myIpMatchingLocation) => console.log(myIpMatchingLocation))
-    .catch(error => { console.error(error); })
-    .finally(() => { console.log(`Location logged`); });
+
+// fetchUserIp().then((ipString) => {
+//     return fetchLocationByIp(ipString);
+// })
+//     .then((locationObject) => {
+//     const locationObjectIp = locationObject.ip;
+//     const locationDetails = locationObject.location;
+//     const locationIsp = locationObject.isp;
+//     return new IpMatchingLocation(locationObjectIp, locationDetails.country, locationDetails.region, locationDetails.city, locationDetails.lat, locationDetails.lng, locationDetails.postalCode, locationDetails.timezone, locationIsp);
+//     //console.log(`Got this data from API`);
+//     //console.log()
+// })
+//     .then((myIpMatchingLocation) => console.log(myIpMatchingLocation))
+//     .catch(error => { console.error(error); })
+//     .finally(() => { console.log(`Location logged`); });
+
+//creating a mock instance of Location to test design and map
+const testLocation = new IpMatchingLocation(
+    '158.222.204.79',
+    `US`,
+    `NY`,
+    `New York`,
+    40.74844205,
+    -73.9856589016075,
+    `10118`,
+    `-04:00`,
+    `Unknown`);
+
+//using Leaflet instructions to add the map
+var ipMatchingMap = L.map('map').setView([testLocation.lat, testLocation.lng], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(ipMatchingMap);
+
+var ipMatchingMarker = L.marker([testLocation.lat, testLocation.lng]).addTo(ipMatchingMap);
+
+
+
+
