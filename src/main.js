df5ad724e174aed6b7df0c5ef494bb42153df5a2
Mariia D <mariia.demidova0304@gmail.com>
2025-07-21 21:03:50 -0400

    Added logic to submit, showing new map and adding data

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const inputForm = document.querySelector(`.input-container`);
 const ipInputField = document.getElementById(`ip-input`);
 const locationDataBox = document.querySelector(`.location-data-container`);
 const mapBox = document.getElementById(`map`);
+var ipMatchingMap;
 
 //added event listener that checks whether we've gotten a location object saved in local storage already
 //if not - we'll save the one we get from user's own IP and load that on page load
@@ -48,46 +49,27 @@ inputForm.addEventListener(`submit`, (event) => {
                 //console.log(`Got this data from API`);
                 //console.log()
             })
-            .then((myIpMatchingLocation) => console.log(myIpMatchingLocation))
+            .then((newIpMatchingLocation) => {
+                //checking that map indeed exists already, then removing previous map and creating a new one
+                if(ipMatchingMap !== undefined){
+                    ipMatchingMap.remove();
+                }
+                    displayMap(newIpMatchingLocation);
+                    displayLocationData(newIpMatchingLocation);
+            })
             .catch(error => { console.error(error); })
-            .finally(() => { console.log(`Location logged`); });
+            .finally(() => { 
+                //clearing the input for IP anyway
+                ipInputField.value = ``;
+             });
     }
 })
 
-//we're going to be fetching user's ip on page load and creating a location object
-// maybe turn creating an object into a reusable function - inside location object as a method?
-
-// fetchUserIp().then((ipString) => {
-//     return fetchLocationByIp(ipString);
-// })
-//     .then((locationObject) => {
-//     const locationObjectIp = locationObject.ip;
-//     const locationDetails = locationObject.location;
-//     const locationIsp = locationObject.isp;
-//     return new IpMatchingLocation(locationObjectIp, locationDetails.country, locationDetails.region, locationDetails.city, locationDetails.lat, locationDetails.lng, locationDetails.postalCode, locationDetails.timezone, locationIsp);
-//     //console.log(`Got this data from API`);
-//     //console.log()
-// })
-//     .then((myIpMatchingLocation) => console.log(myIpMatchingLocation))
-//     .catch(error => { console.error(error); })
-//     .finally(() => { console.log(`Location logged`); });
-
-//creating a mock instance of Location to test design and map
-// const testLocation = new IpMatchingLocation(
-//     '158.222.204.79',
-//     `US`,
-//     `NY`,
-//     `New York`,
-//     40.74844205,
-//     -73.9856589016075,
-//     `10118`,
-//     `-04:00`,
-//     `Unknown`);
 
 //using Leaflet instructions to add the map
 //created a function that takes location instance as a parameter
 function displayMap(locationInstance) {
-    var ipMatchingMap = L.map('map').setView([locationInstance.lat, locationInstance.lng], 13);
+    ipMatchingMap = L.map('map').setView([locationInstance.lat, locationInstance.lng], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
